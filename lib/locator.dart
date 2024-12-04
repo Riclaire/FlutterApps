cffc14250590e251aba684e1d7d808005bffa416
riclaire <extraboost005@gmail.com>
2024-12-04 11:28:39 +0100

    master

---
+++ lib/locator.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Locator extends StatefulWidget {
+  const Locator({super.key});
+
+  @override
+  State<Locator> createState() => _LocatorState();
+}
+
+class _LocatorState extends State<Locator> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
